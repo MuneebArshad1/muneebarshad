a19f04529d1f6faa9732dfcf5ee04eb67cb2ab70
MuneebArshad1 <148101096+MuneebArshad1@users.noreply.github.com>
2023-10-17 11:47:33 +0500

    Add files via upload

---
+++ Q No 2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 12:17:01 2023
+
+@author: LAB
+"""
+a=int(input("enter number"))
+b=1
+i=1
+while i < a+1:
+    b=i*b
+    i=i+1
+print (b)
+    
+    
